efeb17014538c77aa3b3caccb05b2d410ebd521f
alyarctiq <aly.khimji@artiq.ca>
2019-11-26 13:08:08 -0500

    initial

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/alyarctiq/actions-go-mod/randombeer"
+)
+
+func thisbeer(w http.ResponseWriter, r *http.Request) {
+	joke := randombeer.Randombeerrequest{}
+	joke.GetData()
+	t, err := template.ParseFiles("templates/randombeer.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Execute(w, joke)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/beer", thisbeer)
+	http.Handle("/", http.FileServer(http.Dir("./html")))
+}
+
+func main() {
+	fmt.Println("Go Web App Started")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
+
+}
